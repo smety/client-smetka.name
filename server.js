daa3685516fe6181aa06c86c1be1e3c0c084829f
Smety <smetka.tomas90@gmail.com>
2019-08-02 10:14:25 +0200

    Remove commnet

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const app = next({dev: process.env.NODE_ENV !== 'production'})
 const handler = routes.getRequestHandler(app, ({req, res, route, query}) => {
   app.render(req, res, route.page, query)
 })
-// With express
+
 const express = require('express')
 app.prepare().then(() => {
   express().use(handler).listen(3000)
